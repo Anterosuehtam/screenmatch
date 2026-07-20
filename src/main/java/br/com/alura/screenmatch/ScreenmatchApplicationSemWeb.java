61cc6169b1bd9366fde56046bd5b309d4ef6be62
Matheus Antero <101403401+Anterosuehtam@users.noreply.github.com>
2026-07-20 12:16:41 -0300

    feat: faz a migracao da aplicacao para web e cria uma rota para obter series

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplicationSemWeb.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplicationSemWeb.java
@@ -7,13 +7,21 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplicationSemWeb implements CommandLineRunner {
 
     @Autowired
     private SerieRepository serieRepository;
 
 	public static void main(String[] args) {
-		SpringApplication.run(ScreenmatchApplication.class, args);
+		SpringApplication.run(ScreenmatchApplicationSemWeb.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal(serieRepository);
+		principal.exibeMenu();
 	}
 }
+*/
