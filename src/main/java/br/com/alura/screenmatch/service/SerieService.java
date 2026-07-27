3bc3f73509563f1250694f594471e4305289efdb
Matheus Antero <101403401+Anterosuehtam@users.noreply.github.com>
2026-07-27 11:08:47 -0300

    feat: cria metodo para encontrar todas as temporadas de uma determinada serie

--- src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch.service;
 
+import br.com.alura.screenmatch.dto.EpisodioDTO;
 import br.com.alura.screenmatch.dto.SerieDTO;
 import br.com.alura.screenmatch.model.Serie;
 import br.com.alura.screenmatch.repository.SerieRepository;
@@ -54,6 +55,18 @@ public class SerieService {
                     s.getAtores(),
                     s.getPoster(),
                     s.getSinopse());
+        }
+        return null;
+    }
+
+    public List<EpisodioDTO> obterTodasTemporadas(Long id) {
+        Optional<Serie> serie = serieRepository.findById(id);
+
+        if (serie.isPresent()) {
+            Serie s = serie.get();
+            return s.getEpisodios().stream()
+                    .map(e -> new EpisodioDTO(e.getTemporada(), e.getNumeroEpisodio(), e.getTitulo()))
+                    .collect(Collectors.toList());
         }
         return null;
     }
